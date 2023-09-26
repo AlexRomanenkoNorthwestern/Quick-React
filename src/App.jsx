3d04b4fa62b769e12fd9c12f9a83636429dcb968
Alexander Romanenko <alexanderromanenko2025@u.northwestern.edu>
2023-09-25 21:40:57 -0500

    Add_Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Banner from "./components/Banner";
 import CourseList from "./components/CourseList";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -33,7 +35,7 @@ const schedule = {
 
 const App = () => {
   return (
-    <div>
+    <div className = "container">
       <Banner title = {schedule.title}/>
       <CourseList courses = {schedule.courses}/>
     </div>
@@ -41,3 +43,5 @@ const App = () => {
 };
 
 export default App;
+//<CourseCard courses = {schedule.courses.F101}/>
+//<CourseList courses = {schedule.courses}/>
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,12 +1,22 @@
+import './CourseList.css';
+
 const Course = ({course}) => (
-    <text>
-        {course.term}{" CS: "}{course.title}<br/>
-    </text>
+    <div className ="card m-1 p-2">
+      <div className="card-body">
+        <h4>{course.term}{' CS '}{course.number}<br/></h4>
+        {course.title}
+      </div>
+      <div className="card-footer bg-transparent">
+        {course.meets}
+      </div>
+    </div>
 );
 
 const CourseList = ({courses}) => (
-  <div>
-    { Object.entries(courses).map(([id, course]) => <Course key={id} course={course} />) }
+  <div className="course-list">
+    { 
+    Object.entries(courses).map(([id, course]) => <Course key={id} course={course} />) 
+    }
   </div>
 );
 
